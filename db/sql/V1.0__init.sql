dc6fd9eb37ae4ba93d6c7065332b23cdc0ad4c84
y273jian <y273jian@uwaterloo.ca>
2020-12-26 15:04:29 -0500

    updated README.md in db and init.sql

--- db/sql/V1.0__init.sql
+++ db/sql/V1.0__init.sql
@@ -1,17 +1,21 @@
+-- CREATE DATABASE wms;
+
+-- \c wms
+
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS centralHub (
+CREATE TABLE IF NOT EXISTS central_hub (
     ch_id uuid UNIQUE DEFAULT uuid_generate_v4(),
     ip_addr inet,
     PRIMARY KEY (ch_id)
 );
 
-CREATE TABLE IF NOT EXISTS cameraInfo (
+CREATE TABLE IF NOT EXISTS camera_info (
     camera_id uuid UNIQUE DEFAULT uuid_generate_v4(),
     ch_id uuid,
     serial_number uuid UNIQUE,
     latitude DECIMAL,
-    longtitude DECIMAL,
+    longitude DECIMAL,
     altitude DECIMAL,
     setup_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     battery_level INT,
@@ -23,17 +27,17 @@ CREATE TABLE IF NOT EXISTS cameraInfo (
     transmit_power INT,
     last_active_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (camera_id),
-    FOREIGN key (ch_id) REFERENCES centralHub
+    FOREIGN key (ch_id) REFERENCES central_hub
 );
 
-CREATE TABLE IF NOT EXISTS userRole (
+CREATE TABLE IF NOT EXISTS user_role (
     role_id SERIAL,
     role_name VARCHAR,
     description text,
     PRIMARY KEY (role_id)
 );
 
-CREATE TABLE IF NOT EXISTS userInfo (
+CREATE TABLE IF NOT EXISTS user_info (
     user_id uuid UNIQUE DEFAULT uuid_generate_v4(),
     email VARCHAR,
     password VARCHAR NOT NULL,
@@ -43,16 +47,16 @@ CREATE TABLE IF NOT EXISTS userInfo (
     PRIMARY KEY (user_id)
 );
 
-CREATE TABLE IF NOT EXISTS roleOfUser (
+CREATE TABLE IF NOT EXISTS role_of_user (
     role_of_user_id SERIAL,
     user_id uuid,
     role_id INT,
     PRIMARY KEY (role_of_user_id),
-    FOREIGN KEY (user_id) REFERENCES userInfo,
-    FOREIGN KEY (role_id) REFERENCES userRole
+    FOREIGN KEY (user_id) REFERENCES user_info,
+    FOREIGN KEY (role_id) REFERENCES user_role
 );
 
-CREATE TABLE IF NOT EXISTS ImageInfo (
+CREATE TABLE IF NOT EXISTS image_info (
     image_id uuid UNIQUE DEFAULT uuid_generate_v4(),
     file_path text,
     size INT, -- [Bytes]
@@ -61,16 +65,16 @@ CREATE TABLE IF NOT EXISTS ImageInfo (
     taken_camera_id uuid,
     taken_time TIMESTAMP,
     PRIMARY KEY (image_id),
-    FOREIGN KEY (taken_camera_id) REFERENCES cameraInfo
+    FOREIGN KEY (taken_camera_id) REFERENCES camera_info
 );
 
-CREATE TABLE IF NOT EXISTS wildlifeTypes (
-    type_id uuid UNIQUE DEFAULT uuid_generate_v4(),
+CREATE TABLE IF NOT EXISTS wildlife_types (
+    type_id SERIAL,
     name VARCHAR,
     PRIMARY KEY (type_id)
 );
 
-CREATE TABLE IF NOT EXISTS extractedData (
+CREATE TABLE IF NOT EXISTS extracted_data (
     record_id uuid UNIQUE DEFAULT uuid_generate_v4(),
     type_id uuid,
     amount INT,
@@ -78,25 +82,25 @@ CREATE TABLE IF NOT EXISTS extractedData (
     is_viewed BOOLEAN,
     viewed_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (record_id),
-    FOREIGN KEY (type_id) REFERENCES wildlifeTypes
+    FOREIGN KEY (type_id) REFERENCES wildlife_types
 );
 
-CREATE TABLE IF NOT EXISTS correctedData (
+CREATE TABLE IF NOT EXISTS corrected_data (
     corrected_id uuid UNIQUE DEFAULT uuid_generate_v4(),
     amount INT,
     type_id uuid,
     user_id uuid,
     corrected_time TIMESTAMP,
     PRIMARY KEY (corrected_id),
-    FOREIGN KEY (type_id) REFERENCES wildlifeTypes,
-    FOREIGN KEY (user_id) REFERENCES userInfo
+    FOREIGN KEY (type_id) REFERENCES wildlife_types,
+    FOREIGN KEY (user_id) REFERENCES user_info
 );
 
-CREATE TABLE IF NOT EXISTS recordToCorrect (
+CREATE TABLE IF NOT EXISTS record_to_correct (
     record_to_correct_id SERIAL,
     corrected_id uuid,
     record_id uuid,
     PRIMARY KEY (record_to_correct_id),
-    FOREIGN KEY (corrected_id) REFERENCES correctedData,
-    FOREIGN KEY (record_id) REFERENCES extractedData
+    FOREIGN KEY (corrected_id) REFERENCES corrected_data,
+    FOREIGN KEY (record_id) REFERENCES extracted_data
 );
